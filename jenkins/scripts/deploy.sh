4ee5944f7536d40f5ce5afef04b0139a7db02273
lcjj346 <louiscjj@gmail.com>
2024-07-21 16:02:45 +0800

    Add initial files

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\Louis\\OneDrive\\Desktop\\Y2T3\\2216SSD\\Lab\\lab07b\\src:/var/www/html php:7.2-apache
+
 sleep 1
 set +x
 
